7c6e47c363ad97934ce2c15b7b68de075267e24e
Daniel Santa <dani@Daniels-MacBook-Pro.local>
2024-06-25 13:42:36 +0200

    Ports changed

--- src/main/kotlin/com/danielsanta/food/controller/FoodController.kt
+++ src/main/kotlin/com/danielsanta/food/controller/FoodController.kt
@@ -1,7 +1,6 @@
 package com.danielsanta.food.controller
 
 import com.danielsanta.food.model.mapper.FoodMapper.toResponse
-import com.danielsanta.food.model.response.FoodResponse
 import com.danielsanta.food.service.FoodService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +8,5 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class FoodController(private val foodService: FoodService) {
     @GetMapping("/foods")
-    fun getFoods(): List<FoodResponse> {
-        return foodService.getFoods().map { it.toResponse() }
-    }
+    fun getFoods() = foodService.getFoods().map { it.toResponse() }
 }
