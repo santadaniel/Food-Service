51c7f82eafbc00d535b2c04785c3bb99190f42ee
Daniel Santa <dani@Daniels-MacBook-Pro.local>
2024-06-30 21:21:38 +0200

    added annotation

--- src/main/kotlin/com/danielsanta/food/controller/FoodController.kt
+++ src/main/kotlin/com/danielsanta/food/controller/FoodController.kt
@@ -2,10 +2,12 @@ package com.danielsanta.food.controller
 
 import com.danielsanta.food.model.mapper.FoodMapper.toResponse
 import com.danielsanta.food.service.FoodService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = arrayOf("http://localhost:3000"))
 class FoodController(private val foodService: FoodService) {
     @GetMapping("/foods")
     fun getFoods() = foodService.getFoods().map { it.toResponse() }
